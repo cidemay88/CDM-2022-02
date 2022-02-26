088cd5ea0ca7d9090ef2cac880c5b81a2f3c56af
cidem8 <100438017+cidemay88@users.noreply.github.com>
2022-02-26 17:56:06 +0300

    business

---
+++ CSharpApp01/Entities/Concrete/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NationalIdentity { get; set; }
+        public string DateOfBirthYear { get; set; }
+    }
+}
---
+++ CSharpApp01/WorkSpace/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WorkSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            People people = new People(); 
+            Console.ReadLine();
+
+        }
+
+        public class People
+        {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public int BirthDate  { get; set; }
+            public long TcNumber { get; set; }
+
+
+        }
+    }
+}
